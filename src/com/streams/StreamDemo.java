209788aa475c1cbfd44d6a359430362b4eb0f6c5
Nehaa-karthikeyan <nehaakart2004@gmail.com>
2026-02-13 10:45:11 +0530

    [Nehaa] : [Added]:UC 2.3: Collected transformed stream values into a list

--- src/com/streams/StreamDemo.java
+++ src/com/streams/StreamDemo.java
@@ -2,20 +2,28 @@ package com.streams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamDemo {
     public static void main(String[] args) {
 
         System.out.println("Stream API Project Started");
 
-        // UC 2.1: Create stream and iterate elements
+        // UC 2.1
         List<Integer> numbers = Arrays.asList(10, 20, 30, 40, 50);
         numbers.stream().forEach(n -> System.out.println(n));
 
-        // UC 2.2: Transform elements using map
+        // UC 2.2
         numbers.stream()
                 .map(n -> n * 2)
                 .forEach(n -> System.out.println(n));
+
+        // UC 2.3
+        List<Integer> doubledList = numbers.stream()
+                .map(n -> n * 2)
+                .collect(Collectors.toList());
+
+        System.out.println(doubledList);
     }
 }
 

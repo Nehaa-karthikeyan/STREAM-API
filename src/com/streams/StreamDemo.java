ed0710827f28d1133977d81476c82d0b307d7c04
Nehaa-karthikeyan <nehaakart2004@gmail.com>
2026-02-13 10:42:20 +0530

    [Nehaa] : [Added] : UC 2.1: Created stream and iterated elements using forEach

--- src/com/streams/StreamDemo.java
+++ src/com/streams/StreamDemo.java
@@ -1,7 +1,17 @@
 package com.streams;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StreamDemo {
     public static void main(String[] args) {
+
         System.out.println("Stream API Project Started");
+
+        // UC 2.1: Create stream and iterate elements
+        List<Integer> numbers = Arrays.asList(10, 20, 30, 40, 50);
+
+        numbers.stream().forEach(n -> System.out.println(n));
     }
 }
+

5c0469da4faf754b8f2eb942d3ff2e4afd8435e9
Nehaa-karthikeyan <nehaakart2004@gmail.com>
2026-02-13 10:46:55 +0530

    [Nehaa] : [Added]:UC 2.5: Found first even number using findFirst

--- src/com/streams/StreamDemo.java
+++ src/com/streams/StreamDemo.java
@@ -29,7 +29,12 @@ public class StreamDemo {
                 .filter(n -> n % 2 == 0)
                 .collect(Collectors.toList());
         System.out.println(evenNumbers);
+
+        // UC 2.5
+        Integer firstEven = numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .findFirst()
+                .orElse(null);
+        System.out.println(firstEven);
     }
 }
-
-
